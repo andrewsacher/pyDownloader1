f0dc7380e6ba6919273aec939cca5d2cbe3bd9c1
johnricco <johnricco226@gmail.com>
2017-03-07 17:23:10 -0500

    Cleaned up script for readability

--- data_downloader.py
+++ data_downloader.py
@@ -78,24 +78,72 @@ class web_data(object):
     def read_tables(self, crawl_page = False):
         
         self.crawl_page = crawl_page
+        self.pages = list()
+        self.tables = list()
         
         # Crawl page to find all links to pages with tables
         if self.crawl_page == True:
+            for link in self.url_soup.find_all("a"):
+                this_link = link.get("href")
+                if not this_link == None:
+                    if this_link[-5:] == ".html":
+                        self.pages.append(urllib.request.urljoin(self.url, this_link))
             
-            
+        # Else simply use self.url 
+        else:
+            self.pages[0] = self.url
         
+        # Scrape tables 
+        for page in self.pages:
+            try:
+                page_tables = pd.read_html(page, flavor = "bs4", header = 0)
+                for table in page_tables:
+                    self.tables.append(table)
+            except:
+                next
+
+        return(self.tables)
+  
+    # Method to scrape HTML tables and save to disk
+    def save_tables(self, save_path,  crawl_page = False):
+        
+        self.crawl_page = crawl_page
+        self.pages = list()
+        self.tables = list()
+        self.file_names = list()
+        
+        # Crawl page to find all links to pages with tables
+        if self.crawl_page == True:
+            for link in self.url_soup.find_all("a"):
+                this_link = link.get("href")
+                if not this_link == None:
+                    if this_link[-5:] == ".html":
+                        self.pages.append(urllib.request.urljoin(self.url, this_link))
+            
+        # Else simply use self.url 
+        else:
+            self.pages[0] = self.url
+        
+        # Scrape tables
+        for page in self.pages:
+            try:
+                page_tables = pd.read_html(page, flavor = "bs4", header = 0)
+                counter = 1
+                for table in page_tables:
+                    self.tables.append(table)
+                    file_name = os.path.join(self.save_path, page[page.rfind("/") + 1: -5] + "_" + counter + ".csv")
+                    self.file_names.append(file_name)
+                    counter += 1
+            except:
+                next
+        
+        # Save tables
+        for i in range(0, len(self.tables)):
+            self.tables[i].to_csv(self.file_names[i])
 
 
 
 
 
 
-
-
-url      = "https://www.rita.dot.gov/bts/sites/rita.dot.gov.bts/files/publications/national_transportation_statistics/html/table_03_28.html"
-
-
-
-df = pd.read_html(url, flavor = "bs4", header = 0)
-df[0]
 
