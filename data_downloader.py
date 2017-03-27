d6a221fe328c1f039f3e92fe99c329cbeaaa3253
johnricco <johnricco226@gmail.com>
2017-03-27 13:31:53 -0400

    wrap manual table parser into module

--- data_downloader.py
+++ data_downloader.py
@@ -6,23 +6,27 @@ import pandas as pd
 import numpy as np
 from bs4 import BeautifulSoup 
 
-class web_data(object):
+# Import html_tables
+os.chdir("C:\\Users\\jricco\\Documents\\PPI projects\\Ballmer\\pyDownloader1")
+from html_tables import html_tables
+
+# Define downloader class
+class data_downloader(object):
     
     def __init__(self, url):
         
         self.url      = url
         self.r        = requests.get(self.url)
-        self.raw_html = self.r.text
-        self.url_soup = BeautifulSoup(self.raw_html)
-
+        self.url_soup = BeautifulSoup(self.r.text)
+        
     #=======================================
     # Method to download data files to disk
     #=======================================
-    
+        
     def download_files(self, 
-                       save_path, 
-                       ext):
-       
+                   save_path, 
+                   ext):
+   
         self.save_path = save_path
         self.ext = ext
          
@@ -46,19 +50,17 @@ class web_data(object):
         if len(self.failed_links) > 0:
             print("The following files could not be downloaded:")
             for s in self.failed_links:
-                print(s)
-                
+                print(s)    
+    
     #===============================================
     # Method to scrape HTML tables and save to disk
     #===============================================
-    
+        
     def download_tables(self, 
                         save_path, 
                         crawl_page       = True, 
                         page_type        = "html", 
                         row_min          = 1, 
-                        row_shift        = 0, 
-                        record_shifts    = False, 
                         remove_footnotes = True,
                         remove_blanks    = None):
         
@@ -66,14 +68,12 @@ class web_data(object):
         self.crawl_page       = crawl_page
         self.page_type        = page_type
         self.row_min          = row_min
-        self.row_shift        = row_shift
-        self.record_shifts    = record_shifts
         self.remove_footnotes = remove_footnotes
         self.remove_blanks    = remove_blanks
         
-        self.pages       = list()
-        self.tables      = list()
-        self.file_names  = list()
+        self.pages       = []
+        self.tables      = []
+        self.file_names  = []
         
         # Crawl page to find all links to pages with tables
         if self.crawl_page == True:
@@ -85,12 +85,13 @@ class web_data(object):
             
         # Else simply use self.url 
         else:
-            self.pages[0] = self.url
-        
+            self.pages.append(self.url)
+            
         # Scrape tables
         for page in self.pages:
             try:
-                page_tables = pd.read_html(page, flavor = "bs4", header = 0)
+                page_tables_html = html_tables(page)
+                page_tables = page_tables_html.read()
                 counter = 1
                 for table in page_tables:
                     if len(table) >= self.row_min:
@@ -102,48 +103,7 @@ class web_data(object):
                         next
             except:
                 next
-                
-        # Fix rows
-        if self.row_shift != 0:
-            for n in range(0, len(self.tables)):
-                self.shift_index = list()
-                for i in range(0, len(self.tables[n])):
-                    this_row = self.tables[n].iloc[i, ]
-                    if this_row.isnull()[0]:
-                        if all(this_row.isnull()):
-                            self.shift_index.append(0)
-                        else:
-                            self.shift_index.append(self.row_shift)
-                        for j in range(0, len(this_row)):
-                            self.tables[n].iloc[i, j] = this_row.shift(periods = self.row_shift)[j]
-                    else:
-                        self.shift_index.append(0)
-                if all(np.array(self.shift_index) == 0):
-                    next
-                else:
-                    self.tables[n] = self.tables[n].assign(shift = self.shift_index)
-                    
-        # Remove footnotes
-        if self.remove_footnotes:
-            
-            def strip_footnotes(x):
-                if isinstance(x, str):
-                    end = x[-2:]
-                    match_obj = re.match("([0-9])\D", end)
-                    if match_obj:
-                        return(x[:-1])
-                    else:
-                        return(x)
-                else:
-                    return(x)
 
-            for n in range(0, len(self.tables)):
-                for i in range(0, len(self.tables[n].columns)): 
-                    if str(self.tables[n].iloc[:, i].dtype) == "float64":
-                        next
-                    else:
-                        self.tables[n].iloc[:, i] = self.tables[n].iloc[:, i].apply(strip_footnotes)
-                        
         # Remove empty columns
         if self.remove_blanks != None:
             for n in range(0, len(self.tables)):
@@ -152,12 +112,14 @@ class web_data(object):
         # Save tables
         for i in range(0, len(self.tables)):
             self.tables[i].to_csv(self.file_names[i], index = False)
+            
+                    
 
 
 
-      
 
 
-     
-        
-    
+
+
+
+
